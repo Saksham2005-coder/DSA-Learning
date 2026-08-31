7a80c449390b11069d4bdfaf8dd5cadf7f2cdddb
Saksham2005-coder <191314028+Saksham2005-coder@users.noreply.github.com>
2026-08-31 21:35:59 +0000

    Add LeetCode: Single Number III

---
+++ LeetCode/0260-single-number-iii/solution.cpp
@@ -0,0 +1,16 @@
+mp[nums[i]]++;
+
+        vector<int>ans;
+        for(auto it: mp){
+            if(it.second==1)
+            ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
