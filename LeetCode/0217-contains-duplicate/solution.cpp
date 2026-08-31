9c578de23ca646170f999d844ddece8c70449e36
Saksham2005-coder <191314028+Saksham2005-coder@users.noreply.github.com>
2026-08-31 21:42:14 +0000

    Add LeetCode: Contains Duplicate

---
+++ LeetCode/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        
+        for(int i=0;i<n;i++)
+        mp[nums[i]]++;
